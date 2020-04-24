cacf528b2d7dd2b0ea3c0689380435752d710c2e
RizaHKhan <khanriza@gmail.com>
2020-04-24 14:19:46 -0400

    Added feature image option

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function follow_andrew_theme_support()
 {
   add_theme_support('title-tag');
   add_theme_support('custom-logo');
+  add_theme_support('post-thumbnails');
 }
 
 function register_menus()
